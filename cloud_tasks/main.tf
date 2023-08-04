faea7d355a95429d45bdb42a03515d854ad4610c
Prajwal Bhongade <81956745+PrajwalPB@users.noreply.github.com>
2023-08-04 09:40:06 +0530

    Add files via upload

---
+++ cloud_tasks/main.tf
@@ -0,0 +1,128 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+data "aws_caller_identity" "current" {}
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+
+resource "aws_iam_policy" "policy" {
+  name        = var.policy_name
+  description = "My test policy"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "role" {
+  name = var.role_name
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.role.name
+  policy_arn = aws_iam_policy.policy.arn
+}
+
+resource "aws_iam_instance_profile" "profile" {
+  name = var.aws_iam_instance_profile_name
+  role = aws_iam_role.role.name
+}
+
+
+resource "aws_instance" "prajwal_task" {
+  ami           = var.instance_ami
+  instance_type = var.instance_size
+   key_name = "pbsinga"
+   user_data = <<EOF
+   #!/bin/bash
+   BUCKET=my-s3-bucket-prajwal
+   sudo dnf install java-11-amazon-corretto -y
+   wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.91/bin/apache-tomcat-8.5.91.zip
+   sudo unzip apache-tomcat-8.5.91.zip
+   sudo mv apache-tomcat-8.5.91 /mnt/tomcat
+   KEY=`aws s3 ls $BUCKET --recursive | sort | tail -n 1 | awk '{print $4}'`
+   aws s3 cp s3://$BUCKET/$KEY /mnt/tomcat/webapps/
+   sudo chmod 0755 /mnt/tomcat/bin/*
+   sudo ./bin/catalina.sh start
+   EOF
+
+  tags = {
+  Name = var.Name
+  }
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "demoallow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress = [
+    {
+
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    },
+    {
+      description      = "TLS from VPC"
+      from_port        = 8080
+      to_port          = 8080
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+
+
+    }
+  ]
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = var.security_group_name
+  }
+}
---
+++ cloud_tasks/variables.tf
@@ -0,0 +1,33 @@
+variable instance_ami {
+    type = string
+    description = "Server image to use"
+    default = "ami-0a481e6d13af82399"
+}
+
+variable role_name {
+  type = string
+  description = "test role"
+  default = "demorole"
+}
+
+variable instance_size {
+    type = string
+    description = "ec2 web server size"
+    default = "t3.small"
+}
+variable Name {
+  type = string
+  default = "newserver"
+}
+variable policy_name {
+  type = string
+  default = "demopolicy"
+}
+variable aws_iam_instance_profile_name {
+  type = string
+  default = "demoprofile"
+}
+variable security_group_name {
+  type = string
+  default = "demosg"
+}
